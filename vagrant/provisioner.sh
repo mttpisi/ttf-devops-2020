22c8dae5cd5e5fa137241ef36f7c8a8fb57a7be2
mttpisi <matteo.pisi@gmail.com>
2020-01-27 14:03:08 +0100

    provisioner.sh modificato

--- vagrant/provisioner.sh
+++ vagrant/provisioner.sh
@@ -3,8 +3,8 @@
 install_and_configure_git() {
     apk add git git-doc nano
     git config --system core.editor nano
-    su vagrant -c "git config --global user.name \"Gianni Bombelli (bombo82)\""
-    su vagrant -c 'git config --global user.email gianni@giannibombelli.it'
+    su vagrant -c "git config --global user.name \"Matteo Pirovano (mttpisi)\""
+    su vagrant -c 'git config --global user.email matteo.pisi@gmail.com'
 }
 
 install_and_configure_docker() {
